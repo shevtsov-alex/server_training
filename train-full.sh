12515d9d9102ad56a28cc513b0be0ba75c7286cf
Александр Шевцов <aleksandr.shevtsov@playrix.com>
2026-01-28 16:33:51 +0200

    update the lr scheduler.

--- train-full.sh
+++ train-full.sh
@@ -12,11 +12,11 @@ accelerate launch --num_cpu_threads_per_process 1 --mixed_precision bf16 /worksp
     --full_bf16 --fused_backward_pass \
     --timestep_sampling qwen_shift \
     --weighting_scheme none \
-    --optimizer_type adafactor --learning_rate 2e-6 --gradient_checkpointing\
-    --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" "weight_decay=0" \
-    --max_grad_norm 0 --lr_scheduler cosine \
+    --optimizer_type adafactor --learning_rate 4e-6 --gradient_checkpointing\
+    --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" "weight_decay=0.0" \
+    --max_grad_norm 0 --lr_scheduler linear \
     --max_data_loader_n_workers 2 --persistent_data_loader_workers \
-    --max_train_epochs 50 --save_every_n_epochs 10 --seed 42 \
+    --max_train_epochs 150 --save_every_n_epochs 25 --seed 42 \
     --dataset_config /workspace/layered/dataset.toml \
     --output_dir /workspace/layered/model/newfull \
     --output_name layered-full \
